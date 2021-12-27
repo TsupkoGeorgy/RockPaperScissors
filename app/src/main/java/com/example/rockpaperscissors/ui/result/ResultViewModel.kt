0db99480898c38017c5ee66ccbc4fb3ac80c4c54
Skaara9111 <skaara911@gmail.com>
2021-12-27 17:00:33 +0300

    add navigation

---
+++ app/src/main/java/com/example/rockpaperscissors/ui/result/ResultViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.rockpaperscissors.ui.result
+
+import androidx.lifecycle.ViewModel
+
+class ResultViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/rockpaperscissors/ui/title/TitleViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.rockpaperscissors.ui.title
+
+import androidx.lifecycle.ViewModel
+
+class TitleViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
